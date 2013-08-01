83d36edc0a5b4526312c6304769b102accc62820
Zaba <admin@boxbilling.com.pl>
2013-08-01 11:34:46 +0200

    Przymiarka

--- Controller_Admin.php
+++ Controller_Admin.php
@@ -1,25 +1,5 @@
 <?php
-/**
- * Example BoxBilling module
- *
- * LICENSE
- *
- * This source file is subject to the license that is bundled
- * with this package in the file LICENSE.txt
- * It is also available through the world-wide-web at this URL:
- * http://www.boxbilling.com/LICENSE.txt
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@boxbilling.com so we can send you a copy immediately.
- *
- * @copyright Copyright (c) 2010-2012 BoxBilling (http://www.boxbilling.com)
- * @license   http://www.boxbilling.com/LICENSE.txt
- * @version   $Id$
- */
 
-/**
- * This file connects BoxBilling amin area interface and API
- */
 class Box_Mod_ServerStatus_Controller_Admin
 {
 
@@ -28,7 +8,7 @@ class Box_Mod_ServerStatus_Controller_Admin
         return array(
             'subpages'=> array(
                 array(
-                    'location'  => 'system', // place this module in extensions group
+                    'location'  => 'system',
                     'label'     => 'Server Status',
                     'index'     => 1510,
                     'uri'       => 'serverstatus',
@@ -38,22 +18,11 @@ class Box_Mod_ServerStatus_Controller_Admin
         );
     }
 
-    /**
-     * Method to install module
-     *
-     * @return bool
-     */
     public function install()
     {
-        //throw new Box_Exception("Instalacja się nie udała", array(), 123);
         return false;
     }
 
-    /**
-     * Method to uninstall module
-     * 
-     * @return bool
-     */
     public function uninstall()
     {
         return true;
@@ -78,7 +47,6 @@ class Box_Mod_ServerStatus_Controller_Admin
 	public function __curl($url) {
 		  $curl = curl_init();
 		  curl_setopt($curl, CURLOPT_URL, $url);
-		 // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 		  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 		  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 		  curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)"); 
